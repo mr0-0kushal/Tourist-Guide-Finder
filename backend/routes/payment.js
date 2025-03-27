76e7e527c89e8effc59f8de072e83203d3ec23a6
Kushal <kushalsharmahacker678@gmail.com>
2025-03-27 18:08:14 +0530

    Error Fix

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,14 +1,14 @@
 import express from "express";
 import Stripe from "stripe";
 import dotenv from "dotenv";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 dotenv.config();
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // 🟢 Create Stripe Checkout Session
-router.post("/create-checkout-session", verifyUser , verifyToken , async (req, res) => {
+router.post("/create-checkout-session", verifyToken , async (req, res) => {
   try {
     const { guideName, totalPrice } = req.body;
      // ✅ Log incoming request data
